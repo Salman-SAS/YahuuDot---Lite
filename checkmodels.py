19cc202b16529bdc2975895b7d2af6ef428ab868
Salman Abiola Suleiman <53050686+Salman-SAS@users.noreply.github.com>
2026-05-10 15:24:57 +0100

    Update checkmodels.py
    To check model compatibility

--- checkmodels.py
+++ checkmodels.py
@@ -1,6 +1,6 @@
 from google import genai
 
-client = genai.Client(api_key="AIzaSyDvBYNHTE2t7s8zA0IkeCRu6eDQ0U5GxLE")
+client = genai.Client(api_key="Thank you for reading my project ")
 
 for m in client.models.list():
     print(m.name)
